25a1f7f2a4d6465e10c4278bc9ec3df854711c68
thiago miziara <thiagonmiziara@gmail.com>
2021-03-16 18:28:18 -0300

    add components Dashbord e Summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import React from "react";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import {Dashboard} from "./components/Dashboard"
 
 
 
@@ -7,6 +9,7 @@ export function App() {
   return (
     <>
      <Header/>
+     <Dashboard/>
      <GlobalStyle/>
     </>
   );
