3a73eedd3e62d33a215fb063b8f269f4b79b3bfb
thiago miziara <thiagonmiziara@gmail.com>
2021-03-16 18:48:30 -0300

    add component Transaction e estilos

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Container } from "./styles";
 import {Summary} from "../Summary"
+import {TransactionTable}from "../TransactionTable";
 
 export function Dashboard(){
     return(
         <Container>
 
         <Summary/>
+
+        <TransactionTable/>
 
         </Container>
     );
