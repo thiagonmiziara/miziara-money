c43dee8b188d8b1138e29619bb1d7426b78d08a3
thiago miziara <thiagonmiziara@gmail.com>
2021-03-17 19:30:25 -0300

    add a lib react-modal e passando propriedade para o component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+import { useState } from "react";
+import Modal from "react-modal";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import {Dashboard} from "./components/Dashboard"
 
-
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false
+  );
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-     <Header/>
+     <Header onOpenNewTransitionModal={handleOpenNewTransactionModal}/>
      <Dashboard/>
+
+     <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar Transação</h2>
+    </Modal>
      <GlobalStyle/>
     </>
   );
