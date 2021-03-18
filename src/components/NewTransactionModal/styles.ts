d3e267b385922492fc0e3f247d2303c3c0ddac8d
thiago miziara <thiagonmiziara@gmail.com>
2021-03-18 20:14:05 -0300

    add estilos aos buttons entrada e saida no modal

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,93 +1,100 @@
 import styled from "styled-components";
-import {darken} from "polished";
+import { darken, transparentize } from "polished";
 
 export const Container = styled.form`
-
-h2{
-    color:var(--text-title);
+  h2 {
+    color: var(--text-title);
     font-size: 1.5rem;
     margin-bottom: 2rem;
-}
+  }
 
-input{
+  input {
     width: 100%;
     padding: 0 1.5rem;
     height: 4rem;
     border-radius: 0.25rem;
 
-    border: 1px solid #D7D7D7;
-    background: #E7E9EE;
+    border: 1px solid #d7d7d7;
+    background: #e7e9ee;
 
     font-weight: 400;
     font-size: 1rem;
 
-    &::placeholder{
-        color: var(--text-body);
+    &::placeholder {
+      color: var(--text-body);
     }
 
-    & + input{
-        margin-top: 1rem;
+    & + input {
+      margin-top: 1rem;
     }
-}
+  }
 
-    button[type="submit"]{
-        width: 100%;
-        padding: 0 1.5rem;
-        height: 4rem;
-        background:var(--green);
-        color: #fff;
-        border-radius: 0.25rem;
-        border: 0;
-        font-size: 1rem;
-        margin-top: 1.5rem;
-        font-weight: 600;
+  button[type="submit"] {
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 4rem;
+    background: var(--green);
+    color: #fff;
+    border-radius: 0.25rem;
+    border: 0;
+    font-size: 1rem;
+    margin-top: 1.5rem;
+    font-weight: 600;
 
-        transition: filter 0.2s;
+    transition: filter 0.2s;
 
-        &:hover{
-            filter: brightness(0.9);
-        }
+    &:hover {
+      filter: brightness(0.9);
     }
-
-
+  }
 `;
 
 export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+`;
 
-    margin: 1rem 0;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    gap: 0.5rem;
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: "green" | "red";
+}
 
-    button{
-       height: 4rem;
-       border: 1px solid #d7d7d7;
-       border-radius: 0.25rem;
+const colors = {
+  green: "#33cc95",
+  red: "#e52e4d",
+};
 
-       background: transparent;
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
 
-       display: flex;
-       align-items: center;
-       justify-content: center; 
+  background: ${(props) =>
+    props.isActive
+      ? transparentize(0.9, colors[props.activeColor])
+      : "transparent"};
 
-       transition: border-color 0.2s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-       &:hover{
-           border-color: ${darken(0.1,"#d7d7d7")};
-       }
+  transition: border-color 0.2s;
 
-       img{
-           width: 20px;
-           height: 20px;
-       }
+  &:hover {
+    border-color: ${darken(0.1, "#d7d7d7")};
+  }
 
-       span{
-           display: inline-block;
-           margin-left: 1rem;
-           font-size: 1rem;
-           color: var(--text-title);
-       }
-    }
+  img {
+    width: 20px;
+    height: 20px;
+  }
 
-
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
+  }
 `;
